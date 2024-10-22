4fceb632cb094552e821d759b8b7106982b1e047
Soban Shoaib <sobanshoaib7500@gmail.com>
2024-10-22 09:06:35 -0600

    started working on MainActivity.java. For now initialized the buttons from acitivity_main

--- app/src/main/java/com/example/zenithevents/MainActivity.java
+++ app/src/main/java/com/example/zenithevents/MainActivity.java
@@ -1,24 +1,42 @@
 package com.example.zenithevents;
 
 import android.os.Bundle;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button buttonEntrant;
+    Button buttonOrganizer;
+    Button buttonAdmin;
+    Button buttonCreateEventEvent;
+    Button buttonLogIn;
+    Button buttonSignUp;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        buttonEntrant = findViewById(R.id.entrantButton);
+        buttonOrganizer = findViewById(R.id.organizerButton);
+        buttonAdmin = findViewById(R.id.adminButton);
+        buttonCreateEventEvent = findViewById(R.id.createEventButton);
+        buttonLogIn = findViewById(R.id.logInButton);
+        buttonSignUp = findViewById(R.id.signUpButton);
     }
+
+    //    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
+//        setContentView(R.layout.activity_main);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+//    }
 }
