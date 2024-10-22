76d326d437ca9d616a722ecb2863668b91d926f3
realmuaadh <moath.31om@gmail.com>
2024-10-22 15:09:08 -0600

    Created Log In/Sign Up Layout pages, modified mainactivity to implement onClicklistners

--- app/src/main/java/com/example/zenithevents/Entrant.java
+++ app/src/main/java/com/example/zenithevents/Entrant.java
@@ -3,7 +3,21 @@ package com.example.zenithevents;
 import java.util.ArrayList;
 
 public class Entrant {
+    private long entrantId;
+    private String FirstName;
+    private String LastName;
+    private String email;
+    private String phoneNumber;
+    private String password;
+    private String role;
+    // add an user profile image here
+
+
     private ArrayList<Event> entrantEvents;
+
+
+
+
     public void enterWatitingList(Event event) {
         event.getWaitingList().add(this);
         entrantEvents.add(event);
---
+++ app/src/main/java/com/example/zenithevents/LogInActivity.java
@@ -0,0 +1,47 @@
+package com.example.zenithevents;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LogInActivity extends AppCompatActivity {
+
+    private EditText emailEditText, passwordEditText;
+    private Button loginButton, backButton;
+    private TextView signUpTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_log_in);
+
+        emailEditText = findViewById(R.id.Email_editText);
+        passwordEditText = findViewById(R.id.Password_editText);
+        loginButton = findViewById(R.id.LogIn_button);
+        signUpTextView = findViewById(R.id.SignUp_textView);
+        backButton = findViewById(R.id.back_button);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        backButton.setOnClickListener(v -> {
+            finish();
+        });
+
+        signUpTextView.setOnClickListener(v -> {
+            Intent intent = new Intent(LogInActivity.this, SignUpActivity.class);
+            startActivity(intent);
+        });
+
+    }
+}
