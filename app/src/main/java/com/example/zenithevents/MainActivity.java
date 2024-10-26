8a41c85eda8a9067140c3724721e7d5798d8fe2c
realmuaadh <moath.31om@gmail.com>
2024-10-26 16:34:22 -0600

    login/signup is now functional

--- app/src/main/java/com/example/zenithevents/MainActivity.java
+++ app/src/main/java/com/example/zenithevents/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.zenithevents;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,20 +12,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.zenithevents.LogInSignUP.EntrantSignUpActivity;
 import com.example.zenithevents.LogInSignUP.LogInActivity;
 import com.example.zenithevents.LogInSignUP.SignUpOption;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseAuth auth;
     Button buttonEntrant;
     Button buttonOrganizer;
     Button buttonAdmin;
     Button buttonCreateEventEvent;
     Button buttonLogIn;
     Button buttonSignUp;
+    Button buttonLogOut;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance();
 
         buttonEntrant = findViewById(R.id.entrantButton);
         buttonOrganizer = findViewById(R.id.organizerButton);
@@ -31,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
         buttonCreateEventEvent = findViewById(R.id.createEventButton);
         buttonLogIn = findViewById(R.id.logInButton);
         buttonSignUp = findViewById(R.id.signUpButton);
+        buttonLogOut = findViewById(R.id.logOutButton);
 
         buttonLogIn.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, LogInActivity.class);
@@ -40,7 +49,18 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this, SignUpOption.class);
             startActivity(intent);
         });
-    }
+        FirebaseUser currentUser = auth.getCurrentUser();
+        if (currentUser != null) {
+            buttonLogOut.setVisibility(View.VISIBLE);
+    } else {
+            buttonLogOut.setVisibility(View.GONE);
+        }
+        buttonLogOut.setOnClickListener(v -> {
+            auth.signOut();
+            Toast.makeText(MainActivity.this, "Logged Out", Toast.LENGTH_SHORT).show();
+            buttonLogOut.setVisibility(View.GONE);
+        });
+        }
 
 
 }
