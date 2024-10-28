60e56a4754cdeffc235f5c73f6ff0f9b313d2953
aounkhan <maounkhan@icloud.com>
2024-10-27 22:12:07 -0600

    installed Glide

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,8 @@ android {
 
 dependencies {
 
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("annotationProcessor 'com.github.bumptech.glide:compiler:4.15.1'")
     implementation(platform("com.google.firebase:firebase-bom:33.4.0"))
     implementation("com.google.firebase:firebase-firestore")
     implementation(libs.appcompat)
