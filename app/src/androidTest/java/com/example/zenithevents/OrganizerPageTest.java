17001f291d956ec9c97211cd04dc403ab0a713d6
akhafagy47 <akhafagy@ualberta.ca>
2024-11-07 13:03:33 -0700

    Build off of these test files

--- app/src/androidTest/java/com/example/zenithevents/OrganizerPageTest.java
+++ app/src/androidTest/java/com/example/zenithevents/OrganizerPageTest.java
@@ -1,4 +1,39 @@
 package com.example.zenithevents;
 
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.intent.matcher.IntentMatchers;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.example.zenithevents.User.OrganizerPage;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
 public class OrganizerPageTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Before
+    public void setUp() {
+        Intents.init();
+
+    }
+    @After
+    public void tearDown() {
+        Intents.release();
+    }
+
+    @Test
+    public void testOrganizerButton() {
+        Espresso.onView(ViewMatchers.withId(R.id.organizerButton)).perform(ViewActions.click());
+        Intents.intended(IntentMatchers.hasComponent(OrganizerPage.class.getName()));
+    }
 }
--- app/src/androidTest/java/com/example/zenithevents/WaitingListTest.java
+++ app/src/androidTest/java/com/example/zenithevents/WaitingListTest.java
@@ -1,4 +1,40 @@
 package com.example.zenithevents;
 
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.intent.matcher.IntentMatchers;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.example.zenithevents.WaitingListPackage.WaitingList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
 public class WaitingListTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Before
+    public void setUp() {
+        Intents.init();
+
+    }
+    @After
+    public void tearDown() {
+        Intents.release();
+    }
+
+    @Test
+    public void testWaitingListButton() {
+        Espresso.onView(ViewMatchers.withId(R.id.waitingListButton)).perform(ViewActions.click());
+        Intents.intended(IntentMatchers.hasComponent(WaitingList.class.getName()));
+    }
+
 }
