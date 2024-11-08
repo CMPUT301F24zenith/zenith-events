27c1e93355ca7ffdc61900201be653e13dc517bc
Soban Shoaib <sobanshoaib7500@gmail.com>
2024-11-08 15:51:41 -0700

    organizer_testing

---
+++ app/src/androidTest/java/com/example/zenithevents/OrganizerUITest.java
@@ -0,0 +1,47 @@
+package com.example.zenithevents;
+
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.intent.matcher.IntentMatchers;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.example.zenithevents.Events.CreateEventPage;
+import com.example.zenithevents.Facility.CreateFacility;
+import com.example.zenithevents.User.OrganizerPage;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.AllTests;
+
+@RunWith(AndroidJUnit4.class)
+public class OrganizerUITest {
+    @Rule
+    public ActivityScenarioRule<OrganizerPage> scenarioRule = new ActivityScenarioRule<> (OrganizerPage.class);
+
+    @Before
+    public void setUp(){
+        Intents.init();
+    }
+    @After
+    public void tearDown(){
+        Intents.release();
+    }
+
+    @Test
+    public void testCreateEvent(){
+        Espresso.onView(ViewMatchers.withId(R.id.createEventButton)).perform(ViewActions.click());
+        Intents.intended(IntentMatchers.hasComponent(CreateEventPage.class.getName()));
+    }
+    @Test
+    public void testViewFacilityEvent(){
+        Espresso.onView(ViewMatchers.withId(R.id.createFacilityButton)).perform(ViewActions.click());
+        Intents.intended(IntentMatchers.hasComponent(CreateFacility.class.getName()));
+    }
+
+}
