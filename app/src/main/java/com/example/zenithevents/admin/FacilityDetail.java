8605e2c2976974c48ca6f5e0bd48a6edb81f59ed
Soban Shoaib <sobanshoaib7500@gmail.com>
2024-11-20 20:59:58 -0700

    worked on deleting facility

--- app/src/main/java/com/example/zenithevents/admin/FacilityDetail.java
+++ app/src/main/java/com/example/zenithevents/admin/FacilityDetail.java
@@ -1,22 +1,27 @@
 package com.example.zenithevents.admin;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.test.espresso.remote.EspressoRemoteMessage;
 
 import com.example.zenithevents.ArrayAdapters.EventArrayAdapter;
 import com.example.zenithevents.HelperClasses.EventUtils;
 import com.example.zenithevents.Objects.Event;
 import com.example.zenithevents.R;
+import com.google.firebase.Firebase;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -52,6 +57,7 @@ public class FacilityDetail extends AppCompatActivity {
         facilityEmail = findViewById(R.id.facilityEmail);
         facilityPhoneNumber = findViewById(R.id.facilityPhoneNumber);
         facilityEventsListView = findViewById(R.id.facilityEventsListView);
+        deleteFacilityButton = findViewById(R.id.deleteFacility);
 
         eventArrayAdapter = new EventArrayAdapter(this, eventList);
         facilityEventsListView.setAdapter(eventArrayAdapter);
@@ -77,6 +83,46 @@ public class FacilityDetail extends AppCompatActivity {
         facilityEmail.setText(intent.getStringExtra("facilityEmail"));
         facilityPhoneNumber.setText(intent.getStringExtra("facilityPhoneNumber"));
 
+        deleteFacilityButton.setOnClickListener(v-> deleteFacilityConfirmation());
+    }
+
+    private void deleteFacilityConfirmation() {
+        new AlertDialog.Builder(this)
+                .setTitle("Delete Facility")
+                .setMessage("Are you sure you want to delete this facility")
+                .setPositiveButton("Delete", (dialog, which) -> deleteFacilityAndEvents())
+                .setNegativeButton("Cancel", null)
+                .show();
+    }
+
+    private void deleteFacilityAndEvents() {
+        Toast.makeText(this, "Deleting facility and its events", Toast.LENGTH_SHORT).show();
+        eventUtils.fetchOrganizerEvents(this, facilityId, events -> {
+            if (events != null) {
+                for (Event event : events) {
+                    eventUtils.deleteEvent(event.getEventId(), isDelete -> {
+                        if (!isDeleted) {
+                            Toast.makeText(FacilityDetail.this, "Failed to delete", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+            deleteFacility();
+        });
+
+    }
+
+    private void deleteFacility() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("facilities").document(facilityId)
+                .delete()
+                .addOnSuccessListener(aVoid -> {
+                    Toast.makeText(FacilityDetail.this, "Facility deleted", Toast.LENGTH_SHORT).show();
+                    finish();
+                })
+                .addOnFailureListener(e-> {
+                    Toast.makeText(FacilityDetail.this, "Facility failed to delete", Toast.LENGTH_SHORT).show();
+                });
     }
 
 
