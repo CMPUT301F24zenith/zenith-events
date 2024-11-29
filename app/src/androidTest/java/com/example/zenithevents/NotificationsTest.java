2f3ea6988dce61bb48361763f96b07ab48a69362
Abdulrahman Khafagy <akhafagy@ualberta.ca>
2024-11-28 19:36:06 -0700

    Added more tests for notifs

--- app/src/androidTest/java/com/example/zenithevents/NotificationsTest.java
+++ app/src/androidTest/java/com/example/zenithevents/NotificationsTest.java
@@ -1,16 +1,25 @@
 package com.example.zenithevents;
 
 
+import android.content.Context;
+
 import com.example.zenithevents.Objects.User;
 
 import org.junit.Test;
 
 public class NotificationsTest {
+    private Context context;
 
 
     @Test
-    public void testSendNotification() {
+    public void testSendNotificationWinner() {
         User user = new User("deviceID", "firstName", "lastName", "email", "phoneNumber");
-        user.sendNotification("Test message");
+        user.sendNotification(context,"Congratulations! You have been selected for");
+
+    }
+    @Test
+    public void testSendNotificationToAll() {
+        User user = new User("deviceID", "firstName", "lastName", "email", "phoneNumber");
+        user.sendNotification(context,"You have not been selected for");
     }
 }
