d4e19823d9311487a8ee318cd10c037f8710fedd
Soban Shoaib <sobanshoaib7500@gmail.com>
2024-11-28 22:23:38 -0700

    ui for entrant

--- app/src/main/java/com/example/zenithevents/ArrayAdapters/EntrantArrayAdapter.java
+++ app/src/main/java/com/example/zenithevents/ArrayAdapters/EntrantArrayAdapter.java
@@ -81,7 +81,7 @@ public class EntrantArrayAdapter extends ArrayAdapter<User> {
         ImageView profileImage = convertView.findViewById(R.id.profileImage);
         Button acceptBtn = convertView.findViewById(R.id.acceptEntrantBtn);
         Button declineBtn = convertView.findViewById(R.id.declineEntrantBtn);
-
+        androidx.cardview.widget.CardView viewCard = convertView.findViewById(R.id.viewEntrantCard);
         UserUtils userUtils = new UserUtils();
 
         if (user != null) {
@@ -146,7 +146,7 @@ public class EntrantArrayAdapter extends ArrayAdapter<User> {
                 declineBtn.setVisibility(View.GONE);
             }
 
-            convertView.setOnClickListener(v -> {
+            viewCard.setOnClickListener(v -> {
                 if (user.getDeviceID() != null) {
                     Intent intent = new Intent(getContext(), ProfileDetailActivity.class);
                     intent.putExtra("userID", user.getDeviceID());
