6713892a73314b50b8d7b00b3f5257d724278f73
aounkhan <maounkhan@icloud.com>
2024-11-30 19:39:30 -0700

    main screen update

--- app/src/main/java/com/example/zenithevents/MainActivity.java
+++ app/src/main/java/com/example/zenithevents/MainActivity.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
 import com.example.zenithevents.EntrantDashboard.EntrantViewActivity;
 import com.example.zenithevents.User.OrganizerPage;
 import com.example.zenithevents.Admin.AdminViewActivity;
@@ -30,10 +33,8 @@ import com.google.firebase.messaging.FirebaseMessaging;
  * @see FirebaseAuth
  */
 public class MainActivity extends AppCompatActivity {
-    Button buttonEntrant;
-    Button organizerButton;
-    Button buttonAdmin;
-    LinearLayout adminLayout;
+    CardView entrantCard, adminCard, organizerCard;
+    FrameLayout adminLayout;
 
     /**
      * Initializes the activity, sets up button click listeners for navigation
@@ -47,10 +48,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
-        buttonEntrant = findViewById(R.id.entrantButton);
-        organizerButton = findViewById(R.id.organizerButton);
-        buttonAdmin = findViewById(R.id.adminButton);
-        adminLayout = findViewById(R.id.admin_layout);
+        entrantCard = findViewById(R.id.entrantCard);
+        adminCard = findViewById(R.id.organizerCard);
+        organizerCard = findViewById(R.id.adminCard);
+        adminLayout = findViewById(R.id.adminLayout);
 
         FirebaseMessaging.getInstance().subscribeToTopic("news")
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
@@ -64,12 +65,12 @@ public class MainActivity extends AppCompatActivity {
                 });
 
 
-        organizerButton.setOnClickListener(v -> {
+        organizerCard.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, OrganizerPage.class);
             startActivity(intent);
         });
 
-        buttonEntrant.setOnClickListener(v -> {
+        entrantCard.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, EntrantViewActivity.class);
             startActivity(intent);
         });
@@ -89,7 +90,7 @@ public class MainActivity extends AppCompatActivity {
                         Boolean isAdmin = documentSnapshot.getBoolean("isAdmin");
                         if (Boolean.TRUE.equals(isAdmin)) {
                             adminLayout.setVisibility(View.VISIBLE);
-                            buttonAdmin.setOnClickListener(v -> {
+                            adminCard.setOnClickListener(v -> {
                                 Intent intent = new Intent(MainActivity.this, AdminViewActivity.class);
                                 startActivity(intent);
                             });
