37d32016f44e9a2d5ed52ab59da0347c6aa8cd46
Soban Shoaib <sobanshoaib7500@gmail.com>
2024-12-02 00:20:48 -0700

    finished testing browse profiles and events

--- app/src/androidTest/java/com/example/zenithevents/BrowseEventsTestUI.java
+++ app/src/androidTest/java/com/example/zenithevents/BrowseEventsTestUI.java
@@ -2,7 +2,6 @@ package com.example.zenithevents.Admin;
 import androidx.test.espresso.Espresso;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import com.example.zenithevents.MainActivity;
 import com.example.zenithevents.R;
 import org.junit.Rule;
 import org.junit.Test;
@@ -12,21 +11,18 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.action.ViewActions.click;
 
-//need to make sure phone in admin mode
 @RunWith(AndroidJUnit4.class)
-public class BrowseProfilesTestUI {
+public class BrowseEventsTestUI {
 
     @Rule
-    public ActivityScenarioRule<MainActivity> activityScenarioRule = new ActivityScenarioRule<>(MainActivity.class);
+    public ActivityScenarioRule<AdminViewActivity> activityScenarioRule = new ActivityScenarioRule<>(AdminViewActivity.class);
 
     @Test
-    public void GotoAdminProfiles() throws InterruptedException {
-//        time added incase card takes long to load
-        Thread.sleep(2000);
-        Espresso.onView(withId(R.id.adminCard))
+    public void GotoAdminEvents() {
+        Espresso.onView(withId(R.id.btnEvents))
                 .perform(click());
-        Espresso.onView(withId(R.id.listView)).check(matches(isDisplayed()));
-
+        Espresso.onView(withId(R.id.eventsListView))
+                .check(matches(isDisplayed()));
     }
 
 }
